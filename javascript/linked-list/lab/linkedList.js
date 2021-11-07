2db2b865cca1dc1d8d76b117e79b03a7cbca5683
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-11-07 23:03:03 +0200

    ok

--- javascript/linked-list/lab/linkedList.js
+++ javascript/linked-list/lab/linkedList.js
@@ -5,55 +5,69 @@ const Node = require('./Node')
 class LinkedList {
     constructor() {
         this.head = null
+        this.tail = null
     }
 
+
+insertBefore(value, newNode)
+{
+    const node = new Node(newNode)
+    if (!this.head) {
+        return 'error'
+    }
+}
 
 insert(value)
 {
     const node = new Node(value)
+    if (this.head) {
+        node.next = this.head
+    }
+    this.head = node
+}
+
+insertAfter(value, newNode)
+{
+    const node = new Node(newNode)
     if (!this.head) {
-        this.head = node;
-        return this
+        return 'error'
     }
-    let currentNode = this.head
-    while (currentNode.next) {
-        currentNode = currentNode.next
-    }
-    currentNode.next = node
-    return this
 }
 
 include(value)
 {
-    const node = new Node(value)
-    if (!this.head) {
-        this.head = node;
-        return true
-    }
-    else
+    let temp = this.head
+
+    while(temp)
     {
-        return false
+        if(temp.value === value)
+        {
+            return true
+        }
+        temp = temp.next
     }
+    
+}
 
+
+toString()
+{
+    if (!this.head) {
+        return 'linked list is empty'
+    }
+        let currentNode = this.head
+        let finalString=''
+
+    while (currentNode.next)
+     {
+        finalString += `{${currentNode.value}} ->`
+        currentNode = currentNode.next
+    }
+    finalString += `{${currentNode.value}} -> NULL`
+    return finalString
 }
 }
 
-// I tried to work on the string function & it didn't work & I kept trying
 
-// string()
-// {
-//     const node = new Node(null)
-//     let string='hello'
-//     if (!this.head) {
-//         this.head = node;
-//         return this
-//     }
-//         let currentNode = this.head
-//     while (currentNode.next) {
-//         currentNode = currentNode.next
-//     }
-//     currentNode.next = node
-//     return this
-// }
 
 module.exports = LinkedList
