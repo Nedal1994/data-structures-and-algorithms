9d62766c5486b7ce0efe4965b4dbf034a0a344ba
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-11-09 23:52:39 +0200

    ok

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -17,4 +17,17 @@ describe('linked list',()=>{
         list.append('two')
         expect(list.head.value).toEqual('two')
     })
+
+    it('It should return the zipped linked list',()=>{
+        let list=new LinkedList()
+
+        list.insert('1')
+        list.insert('2')
+        list.insert('3')
+        list.insert('4')
+        list.insert('5')
+        list.insert('9')
+        let string = list.string()
+        expect(string).toEqual(' [1] -> [5] -> [3] -> [9] -> [2] -> [4] -> NULL')
+    })
 })
